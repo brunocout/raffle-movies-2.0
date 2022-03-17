710e8a45e40086988b809ac3d8a5280c9d6a2163
Bruno Coutinho <xxbrunomassaxx@gmail.com>
2022-03-17 00:38:59 -0300

    Adjusting layout

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,6 +4,7 @@ const Footer = () => {
     return ( 
         <footer className='footer'>
             <p>A fun and personal project created by <span>Bruno Coutinho</span>. Source code available on Github.</p> 
+            
         </footer>
      );
 }
