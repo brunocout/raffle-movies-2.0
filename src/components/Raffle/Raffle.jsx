949006f89a86d60cc8c384f02d9240337e5231d4
Bruno Coutinho <xxbrunomassaxx@gmail.com>
2022-03-17 18:11:24 -0300

    create responsive layout

--- src/components/Raffle/Raffle.jsx
+++ src/components/Raffle/Raffle.jsx
@@ -6,7 +6,7 @@ const Raffle = () => {
         <div className="raffle">
             <div className="raffle-container">
                 <h1>Choose the movies</h1>
-                <p>Search movies or tv shows</p>
+                <p>Add movies or tv shows</p>
                 <div className='input-container'>
                     <button><RiAddFill /></button>
                     <input type="text" name='search' placeholder='eg. Spider Man' />
