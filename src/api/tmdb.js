aa730d305aeea8735280279d33194d410264692c
Bruno Coutinho <xxbrunomassaxx@gmail.com>
2022-04-11 10:24:55 -0300

    adding dist

--- src/api/tmdb.js
+++
@@ -1,4 +0,0 @@
-export const API_KEY = '392f56ed610e68191dc6dd37d25782a6'
-export const API_BASE = 'https://api.themoviedb.org/3'
-export const API_LANGUAGE = 'pt-BR'
-export const API_IMG = 'https://image.tmdb.org/t/p/original'
